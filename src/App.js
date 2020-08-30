09b2aea919d2826bfa3ea26f184a5d8efb8c4741
ChoiYongWon <yongwon0824@naver.com>
2020-08-30 19:10:45 +0900

    Git branch log test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          CI/CD 끝
+          CI/CD 끝 signin
         </p>
         <a
           className="App-link"
