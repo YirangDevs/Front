5f58f8e981bf0ee341d204b877d531db5414ff72
ChoiYongWon <yongwon0824@naver.com>
2020-09-01 08:15:36 +0900

    Added Content component

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import TopBar from "./TopBar"
 import MainImg from "./MainImg"
+import Content from "./Content"
 
 const Container = () => (
     <>
         <div className="container">
             <TopBar></TopBar>
             <MainImg></MainImg>
+            <Content></Content>
         </div>
     </>
 )
---
+++ src/components/Content.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Content = () =>(
+    <>
+        <div class="content__wrapper">
+            
+        </div>
+    </>
+)
+
+export default Content
