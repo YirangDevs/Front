b75a411046959f95e9d7774a72a219d2461356fc
ChoiYongWon <yongwon0824@naver.com>
2020-09-01 02:49:53 +0900

    Finding yarn error

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          TEST
+          일합시다
         </p>
         <a
           className="App-link"
