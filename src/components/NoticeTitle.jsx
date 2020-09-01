1356bd658f030ef293bab3dc41b1b89ce313c088
ChoiYongWon <yongwon0824@naver.com>
2020-09-01 15:41:19 +0900

    Divided NoticeContent component

---
+++ src/components/NoticeTitle.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const NoticeTitle = () => (
+    <>
+        <div class="content__notice__title">
+            공고글 리스트
+        </div>
+    </>
+)
+
+export default NoticeTitle
