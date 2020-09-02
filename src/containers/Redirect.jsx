3d5425c4f8c188b0e1c6e2d2c6c5237b8c11d901
ChoiYongWon <yongwon0824@naver.com>
2020-09-03 05:54:23 +0900

    Deleted unuse code

--- src/containers/Redirect.jsx
+++ src/containers/Redirect.jsx
@@ -1,11 +1,6 @@
-import React from "react"
 import { connect } from "react-redux"
 import Redirect from "../components/Redirect"
 import ACTION from "../store/actions/action"
-
-const mapStateToProps = (state, ownProps) => {
-    
-}
 
 const mapDispatchToProps = (dispatch) => {
     return {
