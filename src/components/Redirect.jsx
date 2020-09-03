38304da9cff850672733b50c23b30537bfddc35c
ChoiYongWon <yongwon0824@naver.com>
2020-09-04 05:00:41 +0900

    Exec history when login done

--- src/components/Redirect.jsx
+++ src/components/Redirect.jsx
@@ -2,19 +2,21 @@ import React from "react"
 import qs from "qs"
 import _ from "../config/config"
 
-const getUser = async(callback) => {
+const getUser = async(callback, history_callbkack) => {
     window.Kakao.API.request({
         url: '/v2/user/me',
         success: function(res) {
             callback({
                 user : { username : res.properties.nickname }
             })
+            history_callbkack();
         },
         fail: function(error) {
           console.log(
             'login success, but failed to request user information: ' +
               JSON.stringify(error)
           )
+          history_callbkack();
           
         },
       })
@@ -33,7 +35,7 @@ const sendToken = async (ACCESS_TOKEN) => {
 }
 
 const getToken = async (AUTHORIZATION_CODE, callback) => {
-    let payload = "grant_type=authorization_code&client_id="+_.REST_KEY+"&redirect_url="+_.REDIRECT_URL+"&code="+AUTHORIZATION_CODE;
+    let payload = "grant_type=authorization_code&client_id="+_.DEV_REST_KEY+"&redirect_url="+_.DEV_REDIRECT_URL+"&code="+AUTHORIZATION_CODE;
     let data = await fetch(_.KAKAO_TOKEN_URL, {
         method: "POST",
         headers : { 
@@ -54,12 +56,13 @@ const getToken = async (AUTHORIZATION_CODE, callback) => {
     }
 }
 
-const LoginProcess = async (AUTHORIZATION_CODE, logined_callback, set_user_callback) => {
+const LoginProcess = async (AUTHORIZATION_CODE, logined_callback, set_user_callback, history_callbkack) => {
     let token = await getToken(AUTHORIZATION_CODE, logined_callback)
     if(token){
-        await getUser(set_user_callback)
+        await getUser(set_user_callback, history_callbkack)
     }else{
         return false
+        history_callbkack()
     }
 
 }
@@ -69,8 +72,9 @@ const Redirect =  ({location, history, LOGINED, SET_USER})=>{
         ignoreQueryPrefix:true
     })
     if(!query.code) alert("정상적인 접근이 아닙니다.")
-    else LoginProcess(query.code, LOGINED, SET_USER)
-    history.push("/")
+    else LoginProcess(query.code, LOGINED, SET_USER, ()=>{
+        history.push("/")
+    })
     return (
         <>
             
