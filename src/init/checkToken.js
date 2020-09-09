e8fafdd1681e578aa44d476db7b6bcfc817c99f7
cindy9899 <yeonhoo@gmail.com>
2020-09-09 16:16:12 +0900

    YAT decoding ver1

--- src/init/checkToken.js
+++ src/init/checkToken.js
@@ -3,6 +3,11 @@ export default ()=> {
         let ACCESS_TOKEN = localStorage.getItem("YAT")
         if(ACCESS_TOKEN){
             //토큰뜯기 -> 유효기간확인 -> 유효하면 true 안하면 false
+
+            //base64 디코딩
+            const decodedToken = Buffer.from(ACCESS_TOKEN, "base64").toString();
+            console.log(decodedToken); //이건 그냥 적음
+
             resolve(true)
         }
         else throw new Error("Token non exists")
