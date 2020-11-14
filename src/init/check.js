63b770911a4f9c6bdc15e73a1b5f1a062d8d1464
chaeeun <csmo2642@naver.com>
2020-11-14 14:52:47 +0900

    :construction: init

--- src/init/check.js
+++ src/init/check.js
@@ -7,16 +7,8 @@ let check = {
         localStorage.removeItem("SELECT_ID")
         console.log(store.getState().login_reducer.logined)
         console.log(store.getState().user_reducer.role)
-        // if(store.getState().login_reducer.logined && store.getState().user_reducer.role == "ADMIN")
-        // {
-        //     console.log("mange Start")
-        // }
-        // else {
-        //     alert("권한이 없습니다.")
-        //     console.log(store.getState().login_reducer.logined)
-        //     console.log(store.getState().user_reducer.role)
-        //     window.history.back();
-        // };
+        if(store.getState().login_reducer.logined)
+        console.log( console.log(store.getState().login_reducer.logined))
     }
 
 
