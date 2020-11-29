36d4911966223aa6d6e1ce94ac68ed478870a8f4
ChoiYongWon <yongwon0824@naver.com>
2020-11-29 17:55:33 +0900

    init

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,11 +1,12 @@
 import React from "react"
 import styled from "styled-components"
+import PropTypes from 'prop-types';
 
 const ButtonComponent = styled.input.attrs(props=>({type: "button"}))`
     width : ${props=>props.width};
     height: ${props=>props.height};
     background-color: #f1f3f6;
-    font-size: 1.5rem;
+    font-size: 1rem;
     border: 1px solid #ccd4e0;
     color: #707070;
     margin: 2% 0;
@@ -18,5 +19,19 @@ const Button=({width, height, value, onClick})=>(
         </ButtonComponent>
     </>
 )
+
+Button.propTypes = {
+    width : PropTypes.string,
+    height: PropTypes.string,
+    value : PropTypes.string,
+    onClick : PropTypes.func
+}
+
+Button.defaultProps = {
+    width: "auto",
+    height: "auto",
+    value: 'Button',
+    onClick: undefined,
+};
 
 export default Button
