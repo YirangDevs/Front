0b2954459b4c2a7932b52749eab543073b28fbb2
euncherry <csmo2642@naver.com>
2021-01-02 20:55:54 +0900

    :fire:Deleted unused files

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import LoginRedirect from "./containers/home/LoginRedirect"
 import LogoutRedirect from "./containers/home/LogoutRedirect"
 import Manage from "./pages/Manage/index"
 import Create from "./pages/Create/index"
-import Read from "./components/read/Container"
-import Update from "./components/update/Container"
-import ReadAllNotice from './components/readAllNotice/Container'
 import UserAuthority from "./pages/UserAuthority"
 import {BrowserRouter, Switch, Route} from "react-router-dom"
 import Seniors from "./pages/Seniors/index"
@@ -28,13 +25,6 @@ function App() {
         </Route>
         <Route path ="/create" component={Create}>
         </Route> 
-        <Route path ="/read" component={Read}>
-        </Route> 
-        <Route path ="/update" component={Update}>
-        </Route> 
-        <Route path ="/ReadAllNotice" component={ReadAllNotice}>
-        </Route>
-
         <Route path="/userauthority" component={UserAuthority}>
         </Route>
 
--- src/components/atoms/Modal/index.jsx
+++ src/components/atoms/Modal/index.jsx
@@ -6,3 +6,105 @@
  */
 
 
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+
+const ModalWrapper = styled.div`
+  box-sizing: border-box;
+  display: ${(props) => (props.visible ? 'block' : 'none')};
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 1;
+  overflow: auto;
+  outline: 0;
+`
+
+const ModalOverlay = styled.div`
+  box-sizing: border-box;
+  display: ${(props) => (props.visible ? 'block' : 'none')};
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.6);
+  z-index: 0;
+`
+
+const ModalInner = styled.div`
+  box-sizing: border-box;
+  position: relative;
+  box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.5);
+  background-color: #fff;
+  border-radius: 10px;
+  width: auto;
+  max-width: 80%;
+  top: 50%;
+  transform: translateY(-50%);
+  margin: 0 auto;
+  padding: 30px 20px;
+
+`
+const CloseButton = styled.div`
+  display : flex;
+  flex-direction : row-reverse;
+  font-weight: 700; 
+  text-shadow: 0 1px 0 #fff;
+  border : none;
+  background-color: white;
+  cursor: pointer;
+`
+
+function Modal({
+    className,
+    onClose,
+    maskClosable,
+    closable,
+    visible,
+    children,
+}) {
+    const onMaskClick = (e) => {
+        if (e.target === e.currentTarget) {
+            onClose(e)
+        }
+    }
+
+    const close = (e) => {
+        if (onClose) {
+            onClose(e)
+        }
+    }
+    return (
+        <>
+            <ModalOverlay visible={visible} />
+            <ModalWrapper
+                className={className}
+                onClick={maskClosable ? onMaskClick : null}
+                tabIndex="-1"
+                visible={visible}
+            >
+                <ModalInner tabIndex="0" className="modal-inner">
+                    {closable && <CloseButton className="modal-close" onClick={close} >x</CloseButton>}
+                    {children}
+                </ModalInner>
+            </ModalWrapper>
+        </>
+    )
+}
+
+Modal.propTypes = {
+    visible: PropTypes.bool,
+}
+
+Modal.defaultProps = {
+    closable: true,
+    maskClosable: true,
+    visible: false
+}
+
+export default Modal
