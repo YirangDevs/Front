3727750c7442d22a586f7cfd96357c7c0e4493cf
ChoiYongWon <yongwon0824@naver.com>
2021-01-21 21:59:18 +0900

    :lipstick:Add pagination style

--- src/components/atoms/Pagination/index.jsx
+++ src/components/atoms/Pagination/index.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 import styled from "styled-components"
 
 const PaginationWrapper = styled.ul`
@@ -14,9 +14,32 @@ const PaginationItem = styled.li`
     height: 2rem;
     list-style-type: none;
     cursor: pointer;
+    display : flex;
+    justify-content: center;
+    align-items: center;
+    border : solid rgba(255,255,255,0) 1px;
+    border-radius: 5px;
+    ${props=>{
+        return `
+            &:nth-child(`+props.num+`){
+                background-color : #000000;
+                color : #ffffff;
+            }
+        `
+        
+    }
+    }
 `
 
 const Pagination = ({num, onClick}) => {
+
+    const [currentPage, setCurrentPage] = useState(1)
+
+    const pageOnClick = (e) => {
+        onClick(e)
+        setCurrentPage(e.target.innerText)
+    }
+
     const pageNum = []
     for(let i=0;i<num;i++){
         pageNum.push(i+1)
@@ -26,7 +49,7 @@ const Pagination = ({num, onClick}) => {
             <PaginationWrapper>
                 {
                     pageNum.map((i, index)=>{
-                        return <PaginationItem key={index} onClick={onClick}>{pageNum[index]}</PaginationItem>
+                        return <PaginationItem key={index} num={currentPage} onClick={pageOnClick}>{pageNum[index]}</PaginationItem>
                     })
                 }
             </PaginationWrapper>
