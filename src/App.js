f03aaecf4b6393e823bf3b10ced1828e7120de75
ChoiYongWon <yongwon0824@naver.com>
2021-01-28 01:42:25 +0900

    :fire:Fixed Security Bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import NotificationPool from "./containers/redux/components/NotificationPool/";
 import {createGlobalStyle} from "styled-components"
 import YirangRouter from "./router";
@@ -11,19 +11,25 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
+
 function App() {
+  const [isRenew, setIsRenew] = useState(false) //새로 고침 이후 일시적으로 role이 Guest가 되는 증상 해결을 위한 상태
 //단 한번만 실행
   useEffect(()=>{
-    run()
+    run().then((status)=>{
+      if(status){
+        setIsRenew(true)
+      }
+    })
   }, [])
 
-  return (
+  return (isRenew) ? (
     <>
     <GlobalStyle/>
     <NotificationPool/>
     <YirangRouter/>
     </>
-  );
+  ) : null;
 }
 
 export default App;
--- src/init/start.js
+++ src/init/start.js
@@ -5,32 +5,37 @@ import renewToken from "./renewToken"
 
 
 export default () =>{
+    return new Promise((resolve, reject)=>{
         if(!store.getState().login_reducer.logined){
             YAT.exist()
-            .then((YAT)=>{
-                //store.dispatch(ACTION.LOADING_ACTION_FUNC())
-                return renewToken(YAT)
-            })
-            .then((response)=>response.headers.get('Authorization').split(" ")[1])
-            .then((token)=>{
-                
-                let claim = YAT.decode(token)
-                store.dispatch(ACTION.SET_USER__ACTION_FUNC({
-                    user: {
-                        username : claim.username,
-                        imgUrl : claim.imgUrl,
-                        userId : claim.userId,
-                        role : claim.role
-                    }
-                }))
-                
-                store.dispatch(ACTION.LOGIN_ACTION_FUNC());
-            })
-            .catch((err)=>{
-                //store.dispatch(ACTION.LOADING_OUT_ACTION_FUNC())
+                .then((YAT)=>{
+                    //store.dispatch(ACTION.LOADING_ACTION_FUNC())
+                    return renewToken(YAT)
+                })
+                .then((response)=>response.headers.get('Authorization').split(" ")[1])
+                .then((token)=>{
+
+                    let claim = YAT.decode(token)
+                    store.dispatch(ACTION.SET_USER__ACTION_FUNC({
+                        user: {
+                            username : claim.username,
+                            imgUrl : claim.imgUrl,
+                            userId : claim.userId,
+                            role : claim.role
+                        }
+                    }))
+
+                    store.dispatch(ACTION.LOGIN_ACTION_FUNC());
+                })
+                .then(()=>resolve(true))
+                .catch((err)=>{
                 console.log(err)
-                
+
             })
-        }
+    }else{
+        resolve(true)
+    }
+
+    })
     
 }
