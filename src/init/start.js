c841125ce70db144ba185989345d4a6d5eeeb14e
cindy9899 <yeonhoo9899@gmail.com>
2021-01-28 16:12:45 +0900

    :fire:Fixed isRenew state not changing

--- src/init/start.js
+++ src/init/start.js
@@ -7,6 +7,7 @@ import renewToken from "./renewToken"
 export default () =>{
     return new Promise((resolve, reject)=>{
         if(!store.getState().login_reducer.logined){
+            //YAT가 존재, 로그인이 안됬을시 Renew
             YAT.exist()
                 .then((YAT)=>{
                     //store.dispatch(ACTION.LOADING_ACTION_FUNC())
@@ -29,10 +30,13 @@ export default () =>{
                 })
                 .then(()=>resolve(true))
                 .catch((err)=>{
-                console.log(err)
+                    //토큰 만료 됬을시
+                    resolve(true)
+                    console.log(err)
 
             })
     }else{
+        //로그인 됬을시
         resolve(true)
     }
 
