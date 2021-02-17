da160f79808b31a1274ed3666d33ddfdb1e76f6f
cindy9899 <yeonhoo9899@gmail.com>
2021-02-17 15:09:04 +0900

    :zap: Have to fix checkbox and table

--- src/components/organisms/userauthority/Content/index.jsx
+++ src/components/organisms/userauthority/Content/index.jsx
@@ -18,20 +18,6 @@ const TableBoxHeadLists = ["회원등급", "이름", "성별", "연락처", "이
 const authorityTable = ["관리모드"]
 const regionTable = ["지역"]
 
-const userSample = [{authority: "관리자", name: "홍길동", sex: "여", phone: "010-1234-5678", email: "0000@naver.com"},
-                    {authority: "관리자", name: "홍길동", sex: "여", phone: "010-1234-5678", email: "0000@naver.com"},
-                    {authority: "관리자", name: "홍길동", sex: "여", phone: "010-1234-5678", email: "0000@naver.com"},
-                    {authority: "관리자", name: "홍길동", sex: "여", phone: "010-1234-5678", email: "0000@naver.com"},
-                    {authority: "봉사자", name: "홍길동", sex: "남", phone: "010-1234-5678", email: "1234@naver.com"},
-                    {authority: "봉사자", name: "홍길동", sex: "남", phone: "010-1234-5678", email: "1234@naver.com"},
-                    {authority: "봉사자", name: "홍길동", sex: "남", phone: "010-1234-5678", email: "1234@naver.com"},
-                    {authority: "봉사자", name: "홍길동", sex: "남", phone: "010-1234-5678", email: "1234@naver.com"},
-                    {authority: "봉사자", name: "홍길동", sex: "남", phone: "010-1234-5678", email: "1234@naver.com"},
-                    {authority: "봉사자", name: "홍길동", sex: "남", phone: "010-1234-5678", email: "1234@naver.com"}
-                    ]
-const authoritySample = [{authority: "봉사자 변환"},{authority: "봉사자 변환"},{authority: "봉사자 변환"},{authority: "봉사자 변환"},{authority: "관리자 변환"},{authority: "관리자 변환"},{authority: "관리자 변환"},{authority: "관리자 변환"},{authority: "관리자 변환"},{authority: "관리자 변환"}]
-const regionSample = [{region: "수성구,서구"}, {region: "수성구,동구"}, {region: "수성구,북구"}, {region: "달서구,서구"}, {region: "-"},{region: "-"},{region: "-"},{region: "-"},{region: "-"},{region: "-"}]
-
 const UserAuthorityContent = ({
     regionOnClick,
     authorityOnClick,
