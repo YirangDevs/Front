3f30e2400c6ec32d1f743efbd5090e1420b760c4
ChoiYongWon <yongwon0824@naver.com>
2021-04-15 22:33:44 +0900

    :bug:Fixed pageNav role bug

--- src/components/molecules/PageNav/index.jsx
+++ src/components/molecules/PageNav/index.jsx
@@ -45,7 +45,19 @@ const PageMenuNav = ({ role }) => {
                 <Col span={12} >
                     <Divider marginTop={'0px'} marginBottom={'0px'} color={"#000000"} borderWidth={'3.3px'} />
                 </Col>
-
+                {
+                    (path !== '/mypage') ?
+                    <Col span={12}>
+                        <PageNav path={path} block onClick={
+                            () => {
+                                history.push("/mypage")
+                            }
+                        }>
+                            <Typo>마이 페이지</Typo>
+                            <Typo>{'>'}</Typo>
+                        </PageNav>
+                    </Col> : null
+                }
                 {(path !== '/profile') ?
                     <Col span={12}>
 
@@ -77,18 +89,8 @@ const PageMenuNav = ({ role }) => {
                     : null
                 }
 
-                {(role === "VOLUNTEER" && path !== "/mypage") ?
-                    <Col span={12}>
-                        <PageNav path={path} block onClick={
-                            () => {
-                                history.push("/mypage")
-                            }
-                        } >
-                            <Typo>마이 페이지</Typo>
-                            <Typo>{'>'}</Typo>
-                        </PageNav>
-                    </Col>
-                    :
+                {(role === "ADMIN" || role ==="SUPER_ADMIN") ?
+
                     <>
                         {(path !== "/seniors") ?
                             <Col span={12}>
@@ -132,7 +134,7 @@ const PageMenuNav = ({ role }) => {
                                 </Col>
                                 : null
                         }
-                    </>
+                    </> : null
                 }
 
 
--- src/components/organisms/mypage/Content/index.jsx
+++ src/components/organisms/mypage/Content/index.jsx
@@ -182,7 +182,7 @@ const MyPageContent = ({
                                         <>
                                             <Row gutter={[0, 1]} align="flex-start" justify={"space-between"} style={{ marginTop: "10px" }}>
                                                 <Col xs={9} sm={10} md={9} lg={9} xl={10} xxl={10} span={10}>
-                                                    <TableBox primaryKey={"result"} headList={["봉사일시", "장소", "매칭상태", "봉사분야", "신청날짜"]} bodyList={[data]} onClick={() => viewNoticeOnclick(noticeId)} border={"top"}></TableBox>
+                                                    <TableBox primaryKey={"result"} headList={["봉사일시", "장소", "매칭상태", "봉사분야", "신청날짜"]} bodyList={[data]} onClick={() => viewNoticeOnclick(noticeId)} border={"top"} colgroup={[20,20,20,20,20]}></TableBox>
                                                 </Col>
 
                                                 <Col xs={3} sm={2} md={3} lg={3} xl={2} xxl={2} span={2}>
@@ -280,7 +280,7 @@ const MyPageContent = ({
 
 
                         <Row gutter={[0, 0]} align="center" style={{ margin: '0.8rem 0 0 0 ' }}>
-                            <TableBox headList={["봉사일시", "장소", "봉사분야", "신청날짜"]} bodyList={past_body_Lists} border={"top"}></TableBox>
+                            <TableBox headList={["봉사일시", "장소", "봉사분야", "신청날짜"]} bodyList={past_body_Lists} border={"top"} colgroup={[25,25,25,25]}></TableBox>
                         </Row>
 
                         {/* !SECTION 봉사기록조회 */}
