50fc7c90b0ad550c9938a2e8185170f4e64bd1bf
euncherry <csmo2642@naver.com>
2021-05-07 19:25:46 +0900

    :construction: intro page

--- src/components/atoms/Typography/index.jsx
+++ src/components/atoms/Typography/index.jsx
@@ -4,7 +4,11 @@ import styled from 'styled-components'
 
 
 const Typo = styled.div`
+display : flex;
+
+${props => (props.full) ? 'width : 100%; justify-content : inherit' : null};
 font-size : ${props => (props.size) || '1rem'};
+font-family : ${props => (props.fontFamily) || 'Noto Sans CJK KR'};
 color : ${props => (props.color)};
 background-color : ${props => (props.backColor)};
 opacity :  ${props => (props.opacity)};
@@ -14,18 +18,17 @@ border : ${props => (props.border) || null};
 cursor : ${props => (props.cursor) || 'default'};
 
 
-font-family: Noto Sans CJK KR;
 `
 
 
 
 
-const Typography = ({ size, color, onClick, radius,
+const Typography = ({ size, color, onClick, radius, fontFamily, full,
     backColor, children, opacity, weight, cursor, border }) => {
     return (
         <>
-            <Typo size={size} color={color} onClick={onClick}
-                radius={radius} backColor={backColor} weight={weight}
+            <Typo size={size} color={color} onClick={onClick} fontFamily={fontFamily}
+                radius={radius} backColor={backColor} weight={weight} full={full}
                 children={children} opacity={opacity} cursor={cursor} border={border}>
             </Typo>
         </>
--- src/components/organisms/Intro/Content/index.jsx
+++ src/components/organisms/Intro/Content/index.jsx
@@ -2,7 +2,7 @@
  * @author: chaeeun 
  * @Date : 2021-05-04 17:25:02 
  * @Last Modified by: euncherry
- * @Last Modified time: 2021-05-04 20:26:32
+ * @Last Modified time: 2021-05-06 21:14:22
  */
 
 import React from 'react'
@@ -12,6 +12,11 @@ import Row from '../../../../layout/Grid/Row';
 import Col from '../../../../layout/Grid/Column';
 import Image from "../../../atoms/Image/"
 import GreenSignalIcon from "../../../../img/IconGreenSignal.png"
+import HandImage from "../../../../img/HandImage.png"
+import GreenSignalLiterally from "../../../../img/GreenSignalLiterally.svg"
+import OneselfIcon from "../../../../img/OneselfIcon.svg"
+import ContinuousIcon from "../../../../img/ContinuousIcon.svg"
+import PublicIcon from "../../../../img/PublicIcon.svg"
 import Typo from '../../../atoms/Typography';
 
 const IntroContent = ({ role }) => {
@@ -20,21 +25,139 @@ const IntroContent = ({ role }) => {
         <>
             <Video src={MainVideo} width={"100%"} height={"auto"}></Video>
 
+
             <Row justify={"center"}>
-                <Col span={12} justify={"center"} align={'center'} style={{ height: "16.63rem", backgroundColor: "#fc8787" }}>
+                {/* SECTION  ICON */}
+                <Col span={12} justify={"center"} align={'center'} style={{ height: "14.8rem" }}>
                     <Image src={GreenSignalIcon} width={"23.63rem"} height={"auto"}></Image>
                 </Col>
-                <Col span={12} justify={"center"} align={'center'} style={{ height: "16.63rem", background: "linear-gradient( 55deg, #96fd00, #00b700 )" }}>
-                    <Row justify={'center'} align={'start'} style={{ border: "3px black solid" }}>
-                        <Col span={2} justify={'center'} align={'center'}><Typo size={'2rem'} color={"#ffffff"} weight={'bold'}>재가 봉사 가치</Typo></Col>
-                        <Col span={7} >
-                            <Typo size={"1.1rem"} color={"#ffffff"}>자원봉사는 자발,자주,자유의자라는 뜻의 라틴어(voluntas)에서 유래되었고,</Typo>
-                            <Typo size={"1.1rem"} color={"#ffffff"}>자원봉사활동(Voluntarism)이란 개인 및 단체의 자발적 참여와 대가없이 도움이 필요한 이웃과 사회에 </Typo>
-                            <Typo size={"1.1rem"} color={"#ffffff"}>시간과 재능을 제공하여 사회문제 해결 및 사회공익에 기여하는 것을 말하며, 이러한 자원봉사활동을 실전에</Typo>
-                            <Typo size={"1.1rem"} color={"#ffffff"}>옮기는 사람을 자원봉사자(Volunteer)라고 합니다.</Typo>
+                {/* !SECTION  ICON */}
+
+                {/* SECTION  재가봉사 가치 */}
+                <Col span={12} justify={"center"} align={'center'} style={{ height: "14.2rem", background: "linear-gradient( 55deg, #96fd00, #00b700 )" }}>
+                    <Row justify={"center"} align={'center'}>
+                        <Col span={10} justify={"center"} >
+                            <Row justify={'space-between'} align={'start'}>
+                                <Col span={3} justify={'start'} align={'center'}>
+                                    <Typo fontFamily={"Helvetica"} size={'1.9rem'} color={"#ffffff"} weight={'bold'}>재가봉사 가치</Typo>
+                                </Col>
+                                <Col span={9} >
+                                    <Typo size={"1.3rem"} color={"#ffffff"}>자원봉사는 자발,자주,자유의자라는 뜻의 라틴어(voluntas)에서 유래되었고,</Typo>
+                                    <Typo size={"1.3rem"} color={"#ffffff"}>자원봉사활동(Voluntarism)이란 개인 및 단체의 자발적 참여와 대가없이 도움이 필요한 이웃과 사회에 </Typo>
+                                    <Typo size={"1.3rem"} color={"#ffffff"}>시간과 재능을 제공하여 사회문제 해결 및 사회공익에 기여하는 것을 말하며, 이러한 자원봉사활동을 실천에</Typo>
+                                    <Typo size={"1.3rem"} color={"#ffffff"}>옮기는 사람을 자원봉사자(Volunteer)라고 합니다.</Typo>
+                                </Col>
+                            </Row>
                         </Col>
                     </Row>
                 </Col>
+                {/* !SECTION  재가봉사 가치 */}
+
+                {/* SECTION  GreenSignal 의미 */}
+                <Col span={12} justify={"center"} align={'center'} style={{ height: "23rem" }}>
+                    <Row justify={"center"} align={'center'}>
+                        <Col span={8} justify={"center"} align={'center'}>
+                            <Row justify={"space-between"} align={'center'}>
+                                <Col span={6} justify={"center"} align={'center'}>
+                                    <Image src={GreenSignalLiterally} width={"21.31rem"} height={"auto"}></Image>
+                                </Col>
+                                <Col span={5} justify={"start"} align={'center'}>
+                                    <Typo size={"1.2rem"} color={"#008200"}>손길 한잎한잎이 모여서 행복을 만듭니다.</Typo>
+                                    <Typo size={"1.2rem"} color={"#008200"}>희망의 싱그러움이 울리는 방법 '그린시그널'</Typo>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Col>
+                {/* !SECTION  GreenSignal 의미 */}
+
+                {/* SECTION  GreenSignal 의미  Icon */}
+                <Col span={12} justify={"center"} align={'start'} style={{ height: "25rem" }}>
+                    <Row justify={"center"} align={'center'}>
+                        <Col span={4}>
+                            <Row justify={"center"} align={'center'} style={{ height: "19rem", borderRight: "2px rgba(0,0,0,0.31) solid" }}>
+                                <Col span={12} justify={"center"} align={'center'} style={{ paddingTop: "2rem" }} >
+                                    <Image src={OneselfIcon} height={"auto"} width={"4.5rem"}></Image>
+                                </Col>
+                                <Col span={12} justify={"center"} align={'center'}>
+                                    <Typo size={'1.9rem'} color={"#000000"} weight={500}>자발성</Typo>
+                                </Col>
+                                <Col span={12} justify={"center"} align={'center'} style={{ padding: "1rem 0" }}>
+                                    <Typo full size={'1rem'} color={"#000000"} >자신의 의사로써 시간과 재능, 경험을 도움이</Typo>
+                                    <Typo full size={'1rem'} color={"#000000"} >필요한 이웃과 지역사회 공동체 형성에 아무런 </Typo>
+                                    <Typo full size={'1rem'} color={"#000000"} > 대가 없이 활동하는 것입니다.</Typo>
+                                </Col>
+                            </Row>
+                        </Col>
+                        <Col span={4}>
+                            <Row justify={"center"} align={'center'} style={{ height: "19rem", borderRight: "2px rgba(0,0,0,0.31) solid" }}>
+                                <Col span={12} justify={"center"} align={'center'} style={{ paddingTop: "2rem" }} >
+                                    <Image src={ContinuousIcon} height={"auto"} width={"4.5rem"}></Image>
+                                </Col>
+                                <Col span={12} justify={"center"} align={'center'}>
+                                    <Typo size={'1.9rem'} color={"#000000"} weight={500}>지속성</Typo>
+                                </Col>
+                                <Col span={12} justify={"center"} align={'center'} style={{ padding: "1rem 0" }}>
+                                    <Typo full size={'1rem'} color={"#000000"} >자원봉사활동에 참여하게 되면 </Typo>
+                                    <Typo full size={'1rem'} color={"#000000"} >일정기간동안 지속성과 정기적으로</Typo>
+                                    <Typo full size={'1rem'} color={"#000000"} >봉사활동에 참여하는 것을 의미합니다.</Typo>
+                                </Col>
+                            </Row>
+                        </Col>
+                        <Col span={4}>
+                            <Row justify={"center"} align={'center'} style={{ height: "19rem" }}>
+                                <Col span={12} justify={"center"} align={'center'} style={{ paddingTop: "2rem" }}>
+                                    <Image src={PublicIcon} height={"auto"} width={"4.5rem"}></Image>
+                                </Col>
+                                <Col span={12} justify={"center"} align={'center'}>
+                                    <Typo size={'1.9rem'} color={"#000000"} weight={500}>공익성</Typo>
+                                </Col>
+                                <Col span={12} justify={"center"} align={'center'} style={{ padding: "1rem 0" }}>
+                                    <Typo full size={'1rem'} color={"#000000"} >이웃과 지역사회내에 산재하고 </Typo>
+                                    <Typo full size={'1rem'} color={"#000000"} >있는 문제를 해결하여 삶의 질을 향상시키기 </Typo>
+                                    <Typo full size={'1rem'} color={"#000000"} >위하여 활동하는 것을 의미합니다.</Typo>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Col>
+                {/* !SECTION GreenSignal 의미  Icon */}
+
+                {/* SECTION  ABOUT TEAM YIRANG*/}
+                <Col span={12} justify={"flex-end"} align={'center'} style={{ height: "26rem", backgroundImage: `url("${HandImage}" )`, backgroundColor: " #00b700 ", backgroundBlendMode: 'darken', backgroundSize: 'cover' }}>
+                    <Row justify={"center"} align={'center'} style={{ width: "50%", height: "15rem" }}>
+                        <Col span={12} justify={"center"} align={'center'}>
+                            <Typo color={"#ffffff"} weight={'bold'} size={'1.5rem'} fontFamily={"Helvetica"} > ABOUT TEAM YIRANG</Typo>
+                        </Col>
+                        <Col span={12} justify={"center"} align={'center'}>
+                            <Typo color={"#ffffff"} >TeamYirang은<Typo color={"#ffffff"} weight={'bold'}>기획, 프론트앤드, 백앤드, 디자인 팀</Typo>으로 </Typo>
+                            <Typo color={"#ffffff"} >구성하여 만들어 졌으며 언제든지 소통과 컨택의 문이 열려있습니다. </Typo>
+                        </Col>
+                        <Col span={12} justify={"center"} align={'center'}>
+                            <Typo weight={'bold'} size={'1.7rem'} border={'3px black solid'} cursor={'pointer'}> TEAM YIRANG</Typo>
+                        </Col>
+                    </Row>
+                </Col>
+                {/* !SECTION  ABOUT TEAM YIRANG */}
+
+
+                {/* SECTION  Icon BTN*/}
+                <Col span={12} justify={"flex-end"} align={'center'} style={{ height: "26rem", backgroundImage: `url("${HandImage}" )`, backgroundSize: 'cover' }}>
+                    <Row justify={"center"} align={'center'} style={{ width: "50%", height: "100%", backgroundColor: " rgba(0,183,0,0.81) " }}>
+                        <Col span={12} justify={"center"} align={'center'}>
+                            <Typo color={"#ffffff"} weight={'bold'} size={'1.5rem'} fontFamily={"Helvetica"} > ABOUT TEAM YIRANG</Typo>
+                        </Col>
+                        <Col span={12} justify={"center"} align={'center'}>
+                            <Typo color={"#ffffff"} >TeamYirang은<Typo color={"#ffffff"} weight={'bold'}>기획, 프론트앤드, 백앤드, 디자인 팀</Typo>으로 </Typo>
+                            <Typo color={"#ffffff"} >구성하여 만들어 졌으며 언제든지 소통과 컨택의 문이 열려있습니다. </Typo>
+                        </Col>
+                        <Col span={12} justify={"center"} align={'center'}>
+                            <Typo weight={'bold'} size={'1.7rem'} border={'3px black solid'} cursor={'pointer'}> TEAM YIRANG</Typo>
+                        </Col>
+                    </Row>
+                </Col>
+                {/* !SECTION  Icon BTN */}
+
             </Row>
         </>
     )
