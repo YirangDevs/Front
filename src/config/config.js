640d1191e2b96567bfa40f469c4574a5bb4a439b
ChoiYongWon <yongwon0824@naver.com>
2022-04-02 02:39:10 +0900

    env 수정

--- src/config/config.js
+++ src/config/config.js
@@ -1,20 +1,23 @@
-
 export default {
-    DEV_HOST_URL : "http://localhost:3000",
-    DEV_REDIRECT_URL : "http://localhost:3000/login",
-    DEV_LOGOUT_REDIRECT_URL : "http://localhost:3000/logout",
-    DEV_KAKAO_LOGOUT_URL : "https://kauth.kakao.com/oauth/logout?client_id=8a0a75ba5fe2323acaf4a809b83f4ad3&logout_redirect_uri=http://localhost:3000/logout",
-    DEV_KAKAO_AUTHORIZATION_URL : "https://kauth.kakao.com/oauth/authorize?client_id=8a0a75ba5fe2323acaf4a809b83f4ad3&redirect_uri=http://localhost:3000/login&response_type=code",
-    DEV_REST_KEY : "8a0a75ba5fe2323acaf4a809b83f4ad3",
-    DEV_JAVASCRIPT_KEY : "da949a61e4b82f8424efb08ef26d624d",
-    HOST_URL : "https://master.dt2p9zjubkzbo.amplifyapp.com",
-    REDIRECT_URL : "https://master.dt2p9zjubkzbo.amplifyapp.com/login",
-    LOGOUT_REDIRECT_URL : "https://master.dt2p9zjubkzbo.amplifyapp.com/logout",
-    REST_KEY : "00d2c2a6b7c8765f12ab9e1b87c79b56",
-    JAVASCRIPT_KEY : "869920b5e8e018cc1e1abf5232238a9e",
-    AUTHORIZATION_URL : "https://kauth.kakao.com/oauth/",
-    KAKAO_AUTHORIZATION_URL : "https://kauth.kakao.com/oauth/authorize?client_id=00d2c2a6b7c8765f12ab9e1b87c79b56&redirect_uri=https://master.dt2p9zjubkzbo.amplifyapp.com/login&response_type=code",
-    KAKAO_TOKEN_URL : "https://kauth.kakao.com/oauth/token",
-    KAKAO_LOGOUT_URL : "https://kauth.kakao.com/oauth/logout?client_id=00d2c2a6b7c8765f12ab9e1b87c79b56&logout_redirect_uri=https://master.dt2p9zjubkzbo.amplifyapp.com/logout",
-    SERVER_URL : "http://54.180.19.212"
-}
+  DEV_HOST_URL: "http://localhost:3000",
+  DEV_REDIRECT_URL: "http://localhost:3000/login",
+  DEV_LOGOUT_REDIRECT_URL: "http://localhost:3000/logout",
+  DEV_KAKAO_LOGOUT_URL:
+    "https://kauth.kakao.com/oauth/logout?client_id=8a0a75ba5fe2323acaf4a809b83f4ad3&logout_redirect_uri=http://localhost:3000/logout",
+  DEV_KAKAO_AUTHORIZATION_URL:
+    "https://kauth.kakao.com/oauth/authorize?client_id=8a0a75ba5fe2323acaf4a809b83f4ad3&redirect_uri=http://localhost:3000/login&response_type=code",
+  DEV_REST_KEY: "8a0a75ba5fe2323acaf4a809b83f4ad3",
+  DEV_JAVASCRIPT_KEY: "da949a61e4b82f8424efb08ef26d624d",
+  HOST_URL: "http://54.180.19.212",
+  REDIRECT_URL: "http://54.180.19.212/login",
+  LOGOUT_REDIRECT_URL: "http://54.180.19.212/logout",
+  REST_KEY: "00d2c2a6b7c8765f12ab9e1b87c79b56",
+  JAVASCRIPT_KEY: "869920b5e8e018cc1e1abf5232238a9e",
+  AUTHORIZATION_URL: "https://kauth.kakao.com/oauth/",
+  KAKAO_AUTHORIZATION_URL:
+    "https://kauth.kakao.com/oauth/authorize?client_id=00d2c2a6b7c8765f12ab9e1b87c79b56&redirect_uri=http://54.180.19.212/login&response_type=code",
+  KAKAO_TOKEN_URL: "https://kauth.kakao.com/oauth/token",
+  KAKAO_LOGOUT_URL:
+    "https://kauth.kakao.com/oauth/logout?client_id=00d2c2a6b7c8765f12ab9e1b87c79b56&logout_redirect_uri=http://54.180.19.212/logout",
+  SERVER_URL: "http://54.180.19.212",
+};
